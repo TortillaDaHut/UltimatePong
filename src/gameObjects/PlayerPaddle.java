b5fd45f58c6fef9215ceb68a25c24b4bae6aef9a
varnereh <161881706+varnereh@users.noreply.github.com>
2024-04-28 23:11:52 -0400

    Update PlayerPaddle.java
    Updated PlayerPaddle class to support randomly selected sprites for the player paddle

--- src/gameObjects/PlayerPaddle.java
+++ src/gameObjects/PlayerPaddle.java
@@ -1,10 +1,21 @@
 package gameObjects;
 
 import java.awt.*;
+import java.util.Random;
+import javax.swing.*;
 
 public class PlayerPaddle extends Paddle {
+    
+    // Instantiation of images as global variables
+    Image[] images = new Image[3];
+    Random random = new Random();
+    int randomNum = random.nextInt(3);
+    
        public PlayerPaddle(PongGame pongGame) {
         // No need to initialize pongGame or addKeyListener anymore
+           images[0] = new ImageIcon("./goldPaddleImg.png").getImage();
+           images[1] = new ImageIcon("./hotDogImg.png").getImage();
+           images[2] = new ImageIcon("./capsuleImg.png").getImage();
     }
 
     // Method to continuously check for key presses and update paddle position
@@ -18,8 +29,7 @@ public class PlayerPaddle extends Paddle {
     }
 
     public void paints(Graphics g) {
-        super.paint(g);
-        g.setColor(Color.BLUE);
-        g.fillRect(0, y, width, height);
+        super.paintComponent(g);
+        g.drawImage(images[randomNum], 0, y, null);
     }
 }
