dc36f2bc97dbe1c80b518523d519301c9a59886e
varnereh <161881706+varnereh@users.noreply.github.com>
2024-04-28 23:15:13 -0400

    Update Sound.java
    Updated Sound class to support more sounds

--- src/gameObjects/Sound.java
+++ src/gameObjects/Sound.java
@@ -4,24 +4,29 @@ import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
 public class Sound {
-
-	File[] sounds = new File[30]; // array of all sounds
-
-	// Sound constructor
-	public Sound() {
-		sounds[0] = new File("ballBounce.wav");
-		// add other sounds in sounds[n] = new File("filename");
-	}
-
-	public void playSound(int soundIndex) {
-		try {
-			Clip clip = AudioSystem.getClip();
-			clip.open(AudioSystem.getAudioInputStream(sounds[soundIndex]));
-			clip.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-
+    
+    File[] sounds = new File[30]; // array of all sounds
+    
+    // Sound constructor
+    public Sound() {
+        sounds[0] = new File("ballBounce.wav");
+        sounds[1] = new File("pause.wav");
+        sounds[2] = new File("unpause.wav");
+        sounds[3] = new File("pressButton.wav");
+        // add other sounds in sounds[n] = new File("filename");
+    }
+        
+    public void playSound(int soundIndex) {
+        try {
+            Clip clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(sounds[soundIndex]));
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+   }
+    
+    
 }
+    
+
