93ed169892d0bf1a4bfcd433d6347991b2317fe1
Wolfslide <161881625+Wolfslide@users.noreply.github.com>
2024-04-30 22:42:02 -0400

    Update Main.java

--- src/gameObjects/Main.java
+++ src/gameObjects/Main.java
@@ -9,7 +9,6 @@ public class Main {
 
 	public static void main(String[] args) {
         
-		//JFrame stuff I got off internet and I didn't change any of it
 		JFrame frame = new JFrame("Ultimate Pong");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().add(new PongGame());
